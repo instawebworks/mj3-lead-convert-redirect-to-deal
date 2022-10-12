80b5920e1049537ddb8932ca8983485696142ee3
Insta Web Works <59417571+instawebworks@users.noreply.github.com>
2022-10-12 12:49:34 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,15 @@ function App() {
              * Convert Lead
              */
             let request = {
-                url: `https://www.zohoapis.com/crm/v2/functions/bcrm_convert_lead_from_widget/actions/execute?auth_type=apikey&zapikey=1003.bb0f6891a2718fb7f54f39f970347d49.b025ed1ad7ca15b0a6acd464966a8318&lead_id=${entityId}`,
+                url: `https://www.zohoapis.com/crm/v2/functions/bcrm_convert_lead_from_widget/actions/execute?auth_type=apikey&zapikey=1003.6bcce2b1bc610235dc15304af47808c9.a6daa398db38f2535e53970810d71869&lead_id=${entityId}`,
             };
-            ZOHO.CRM.HTTP.get(request).then(function (data) {
-                const accountId = JSON.parse(data)?.Account_ID;
-                if (accountId) {
+            ZOHO.CRM.HTTP.get(request).then(async function (data) {
+                console.log({ data });
+                const dealId = JSON.parse(data)?.Deal_ID;
+                if (dealId) {
+                    await ZOHO.CRM.BLUEPRINT.proceed();
                     window.open(
-                        `https://crm.zoho.com/crm/org2248734/tab/Accounts/${accountId}`,
+                        `https://crm.zoho.com/crm/org37143442/tab/Potentials/${dealId}`,
                         "_top"
                     );
                 } else {
@@ -40,7 +42,7 @@ function App() {
     return (
         <>
             <h2>
-                Lead is converting and redirecting to Account. Please wait......
+                Lead is converting and redirecting to Deal. Please wait......
             </h2>
             {/* <button
                 onClick={() => {
